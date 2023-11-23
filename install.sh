6547e47068b88b8b1870a2e961832daffb0bc076
Eduardo Barroso <git@edbar.xyz>
2023-11-23 00:00:48 -0300

    Adds picom config

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@
 rm -rf "$HOME/.config/i3/"
 ln -s "$HOME/dotfiles/i3/" "$HOME/.config"
 
+#########
+# picom #
+#########
+
+rm -rf "$HOME/.config/picom/"
+ln -s "$HOME/dotfiles/picom/" "$HOME/.config"
