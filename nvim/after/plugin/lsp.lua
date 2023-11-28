7dd74b9d4eac354aa143473d66881a7a9e5d6ff7
Eduardo Barroso <git@edbar.xyz>
2023-11-27 22:53:56 -0300

    Enables inline diagnostics

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,6 +7,10 @@ lsp.on_attach(function(client, bufnr)
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 end)
-
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true
+}
+)
 lsp.setup()
 
--- nvim/after/plugin/trouble.lua
+++
@@ -1,7 +0,0 @@
-require('trouble').setup {
-    height = 5,
-    icons = false,
-    mode = "document_diagnostics",
-    use_diagnostic_sign = false,
-
-}
